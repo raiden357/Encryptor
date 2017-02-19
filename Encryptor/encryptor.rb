c24d39915e96a1a73f2c6dca277367295c92d579
Ryan Burrow <ryanrip333@yahoo.com>
2017-02-19 18:42:29 -0500

    new methods
    added encrypt(string) method

--- Encryptor/encryptor.rb
+++ Encryptor/encryptor.rb
@@ -10,8 +10,21 @@ class Encryptor
      'y'=>'l','z'=>'m',}
    end
 
-   def encrypt(letter)   # creates new method which encrypts the message
-     lowercase_letter = letter.downcase
-     cipher[letter]
+   def encrypt_letter(letter)   # creates new method which encrypts the message
+     lowercase_letter = letter.downcase # automatically changes letters to lowercase
+     cipher[lowercase_letter]
    end
- end
+
+   def encrypt (string)
+     letters = string.split("") # 1. Split input string into letters
+
+     # 2. Encrypt each letter one at a time, storing the results in an array
+     results = []
+     letters.each do |letter|
+       encrypted_letter = encrypt_letter(letter)
+       results.push(encrypted_letter)
+     end
+
+     # 3. Merge the results back togetther in one string
+     results.join
+   end
